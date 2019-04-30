c8645ba90db5fb9a87a6b84b0365f6593a3a079e
KINGDAN00 <danamphred@gmail.com>
2019-04-30 16:10:21 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:kibabi_market/ecommerce/ecommerce1.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'KIBABI MARKET',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: EcommerceOnePage(),
+    );
+  }
+}
